f8be8a9c91a835e536a9eb220b582111393aefd0
ruaruarua <2750979957@qq.com>
2019-08-09 21:04:49 +0800

    updata

--- src/utils/fetch_api.js
+++ src/utils/fetch_api.js
@@ -1,6 +1,6 @@
 
 function myFetch (url,body) {
-  return fetch("http://127.0.0.1:2333/"+url,body)
+  return fetch("http://39.105.160.149/"+url,body)
   .then((res)=>{
     return res.json()
   }).then((res)=>{
