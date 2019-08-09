9fe812f8526d8c5fdd36212462c0bda988cf9dee
fanrax <2750979957@qq.com>
2019-08-09 17:16:53 +0800

    updata

---
+++ build/precache-manifest.a7c339369bb41fe1a63f28d407fdfb2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c8e8ba18a1838dbc9fe62a7c29d4e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90f25737d3aed83c5b12",
+    "url": "/static/css/2.1d6907fe.chunk.css"
+  },
+  {
+    "revision": "9cfca8c9f094ca9e84a8",
+    "url": "/static/css/main.7ed9ab32.chunk.css"
+  },
+  {
+    "revision": "90f25737d3aed83c5b12",
+    "url": "/static/js/2.a55a3789.chunk.js"
+  },
+  {
+    "revision": "9cfca8c9f094ca9e84a8",
+    "url": "/static/js/main.4fa66b37.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
